d47305737c21661ac62cc8b7d7a898db6a80efa1
000deepak <kumbhardeepak000@gmail.com>
2021-12-21 09:30:56 +0530

    methods

---
+++ Boolean.js
@@ -0,0 +1,5 @@
+Boolean Methods
+
+toSource()	returns the source of Boolean object as a string.
+toString()	converts Boolean into String.
+valueOf()	converts other type into Boolean.
---
+++ Math.js
@@ -0,0 +1,30 @@
+Math Methods	
+
+abs()	It returns the absolute value of the given number.
+log()	It returns natural logarithm of a number.
+exp()	It returns the exponential form of the given number.
+
+cbrt()	It returns the cube root of the given number.
+sqrt()	It returns the square root of the given number
+hypot()	It returns square root of sum of the squares of given numbers.
+
+ceil()	It returns a smallest integer value, greater than or equal to the given number.
+floor()	It returns largest integer value, lower than or equal to the given number.
+round()	It returns closest integer value of the given number.
+trunc()	It returns an integer part of the given number.
+
+max()	It returns maximum value of the given numbers.
+min()	It returns minimum value of the given numbers.
+pow()	It returns value of base to the power of exponent.
+random()It returns random number between 0 (inclusive) and 1 (exclusive).
+sign()	It returns the sign of the given number
+
+sin()	It returns the sine of the given number.
+sinh()	It returns the hyperbolic sine of the given number.
+acos()	It returns the arccosine of the given number in radians.
+asin()	It returns the arcsine of the given number in radians.
+atan()	It returns the arc-tangent of the given number in radians.
+cos()	It returns the cosine of the given number.
+cosh()	It returns the hyperbolic cosine of the given number.
+tan()	It returns the tangent of the given number.
+tanh()	It returns the hyperbolic tangent of the given number.
